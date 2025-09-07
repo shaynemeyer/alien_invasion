0ab2ae132305deed20c314b2e4700ba49a5eb4e4
Shayne Meyer <shayne.meyer@gmail.com>
2025-09-07 06:28:30 -0700

    added a start button

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,8 +33,8 @@ class AlienInvasion:
 
         self._create_fleet()
 
-        # Start Alien Invasion in an active state.
-        self.game_active = True
+        # Start Alien Invasion in an inactive state.
+        self.game_active = False 
 
     def run_game(self):
         """Start the main loop for the game."""
